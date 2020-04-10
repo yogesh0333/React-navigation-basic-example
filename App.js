5d8bc3cb1491f5df1283fb7bd6a127dd3c176f89
yogesh kumar <yogesh@apcompinfotech.com>
2020-04-10 17:30:04 +0530

    React Native basic navigation exmple

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Button, View, Text} from 'react-native';
+import {Button, View, Text, StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
@@ -7,10 +7,12 @@ function HomeScreen({navigation}) {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Home Screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
-      />
+      <View style={styles.button}>
+        <Button
+          title="Go to Details"
+          onPress={() => navigation.navigate('Details')}
+        />
+      </View>
     </View>
   );
 }
@@ -19,16 +21,27 @@ function DetailsScreen({navigation}) {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Details Screen</Text>
-      <Button
-        title="Go to Details... again"
-        onPress={() => navigation.push('Details')}
-      />
-      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
-      <Button title="Go back" onPress={() => navigation.goBack()} />
-      <Button
-        title="Go back to first screen in stack"
-        onPress={() => navigation.popToTop()}
-      />
+      <View style={styles.button}>
+        <Button
+          title="Go to Details... again"
+          onPress={() => navigation.push('Details')}
+        />
+      </View>
+      <View style={styles.button}>
+        <Button
+          title="Go to Home"
+          onPress={() => navigation.navigate('Home')}
+        />
+      </View>
+      <View style={styles.button}>
+        <Button title="Go back" onPress={() => navigation.goBack()} />
+      </View>
+      <View style={styles.button}>
+        <Button
+          title="Go back to first screen in stack"
+          onPress={() => navigation.popToTop()}
+        />
+      </View>
     </View>
   );
 }
@@ -47,3 +60,9 @@ function App() {
 }
 
 export default App;
+
+const styles = StyleSheet.create({
+  button: {
+    margin: 10,
+  },
+});
