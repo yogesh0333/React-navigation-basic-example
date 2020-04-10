23344b5dddbe1b0bd48dbd3686c3ef385bb323b4
yogesh kumar <yogesh@apcompinfotech.com>
2020-04-10 17:19:33 +0530

    React Native basic navigation exmple

---
+++ login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Login = () => {
+  return (
+    <View>
+      <Text>Login Login</Text>
+    </View>
+  );
+};
+
+export default Login;
---
+++ register.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const register = () => {
+  return (
+    <View>
+      <Text>register</Text>
+    </View>
+  );
+};
+
+export default register;
